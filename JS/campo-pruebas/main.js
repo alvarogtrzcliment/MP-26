3afb37cf2babcee3216d0aa1f62eb1d285a3c8c1
Álvaro Gutiérrez <124072319+alvarogtrzcliment@users.noreply.github.com>
2026-02-04 19:55:49 +0100

    04 - 02

---
+++ JS/campo-pruebas/main.js
@@ -0,0 +1,11 @@
+console.log('Hola desde el otro lado')
+
+let cocheAzul = 'Coche Azul'
+
+cocheAzul = 'Coche Verde'
+
+console.log(cocheAzul)
+
+const arrayCoches = ['cocheAzul', 'cocheVerde', 4]
+
+console.log(arrayCoches[0])
