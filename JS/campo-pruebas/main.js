86d6a014305db8454eba38a0bd3acf8e7111a8fb
Álvaro Gutiérrez <124072319+alvarogtrzcliment@users.noreply.github.com>
2026-02-05 19:57:51 +0100

    05 - 02

--- JS/campo-pruebas/main.js
+++ JS/campo-pruebas/main.js
@@ -9,3 +9,28 @@ console.log(cocheAzul)
 const arrayCoches = ['cocheAzul', 'cocheVerde', 4]
 
 console.log(arrayCoches[0])
+
+// Condicionales
+
+const numeroPeces = 17
+
+if (numeroPeces <= 4) {
+  console.log(`Tienes ${numeroPeces} que son menos de 4 peces`)
+}
+
+//  Bucles
+
+for (let contador = 0; contador < 6; contador = contador + 1) {
+  console.log(`Estoy en la iteración número ${contador}`)
+}
+// Bucles for of
+
+const semana = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes']
+
+for (const dia of semana) {
+  console.log(dia)
+}
+
+for (const posicionDia in semana) {
+  console.log(posicionDia)
+}
